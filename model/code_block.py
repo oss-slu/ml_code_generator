d49d7bb40823c44e8e8076a13fa0ad2393cd00f1
Kate Holdener <kate.holdener@slu.edu>
2022-02-21 15:54:51 -0600

    Added next block to CodeBlock

--- model/code_block.py
+++ model/code_block.py
@@ -2,9 +2,16 @@ class CodeBlock:
    def __init__(self, comment, statements):
       self.comment = comment
       self.statements = statements
+      self.next_block = None
 
    def to_text(self):
-      text_value = comment+"\n"
-      for statement in statements:
+      text_value = self.comment+"\n"
+      for statement in self.statements:
          text_value += statement+"\n"
       return text_value
+
+   def set_next_block(self, next_block):
+      self.next_block = next_block
+
+   def get_next_block(self):
+      return self.next_block
