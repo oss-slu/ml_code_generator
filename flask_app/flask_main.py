47b284efc7520f29d8ba01abf2d2d28bd8a8c734
Kate Holdener <kate.holdener@slu.edu>
2022-02-22 09:26:42 -0600

    added two UIs

---
+++ flask_app/flask_main.py
@@ -0,0 +1,64 @@
+from application import code_generator
+
+import os
+from flask import g
+from flask import Flask
+from flask import render_template
+from flask import request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'csv'}
+
+app = Flask(__name__)
+UPLOAD_FOLDER='../data/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+generator = code_generator.CodeGenerator()
+
+@app.route('/')
+def welcome():
+   return "Welcome"
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/download', methods=['GET'])
+def download_code():
+   code = generator.download_code()
+   return render_template('code.html', text=code)
+
+@app.route('/describe', methods=['GET'])
+def describe_data():
+   description = generator.describe_data()
+   return render_template('description.html',table=description.to_html())
+
+@app.route('/data', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #    return redirect(url_for('download_file', name=filename))
+            print(g)
+            with app.app_context():
+               generator.load_data(app.config['UPLOAD_FOLDER']+'/'+filename)
+            return render_template('actions.html')
+
+    return render_template('upload_data.html')
+
+# main driver function
+if __name__ == '__main__':
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
---
+++ main.py
@@ -0,0 +1,13 @@
+from application import code_generator
+import sys
+
+print(sys.argv)
+input_file = sys.argv[1]
+generator = code_generator.CodeGenerator()
+generator.load_data(input_file)
+data_summary = generator.describe_data()
+print(data_summary.to_csv())
+print(generator.get_labels())
+
+code = generator.download_code();
+print(code)
