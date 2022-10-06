f3f97aaff3831428911ec195c5bf4261e0c3be53
Kate Holdener <kate.holdener@slu.edu>
2022-10-06 15:52:42 -0500

    fixed indentation issue

--- pandas_code/code_templates/train_model.py
+++ pandas_code/code_templates/train_model.py
@@ -3,9 +3,9 @@ from sklearn.linear_model import LinearRegression
 #import numpy as np
 
 def get_code(args):
-  print("args[0] ", args[0])
-  args[0] = [args[0]]
-  #args[0] = np.reshape(81, 0)
+   print("args[0] ", args[0])
+   args[0] = [args[0]]
+   #args[0] = np.reshape(81, 0)
    #args[1] = np.reshape(-1, 1)
    model = LinearRegression().fit(args[0], args[1])
    return model
