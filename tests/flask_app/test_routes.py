f4985c4cb9c8c15890146a7b313a90aede68d2eb
Tyler McMahon <tyler.mcmahon@slu.edu>
2022-10-14 02:45:57 +0000

    GET methods work

--- tests/flask_app/test_routes.py
+++ tests/flask_app/test_routes.py
@@ -3,6 +3,7 @@ import pytest
 
 from flask_app.flask_main import create_app
 
+
 @pytest.fixture(name="client")
 def fixture_client():
    app = create_app()
@@ -27,8 +28,59 @@ def test_download_get_method(client):
    assert response.status_code == 200
 
 def test_clean_get_method(client):
-   data = {
-      'file': (io.BytesIO(b"some random data"), "test_data.fake")
-   }
-   response = client.get('/clean', data=data)
+   csv = "tests/flask_app/test_data/fake_data.csv"
+   csv_data = open(csv, "rb")
+   data = {"file": (csv_data, "sample_data.csv")}
+   response = client.post('/data', data=data)
+   response = client.get('/clean')
    assert response.status_code == 200
+
+def test_describe_get_method(client):
+   csv = "tests/flask_app/test_data/fake_data.csv"
+   csv_data = open(csv, "rb")
+   data = {"file": (csv_data, "sample_data.csv")}
+   response = client.post('/data', data=data)
+   response = client.get('/describe')
+   assert response.status_code == 200
+
+def test_split_get_method(client):
+   csv = "tests/flask_app/test_data/fake_data.csv"
+   csv_data = open(csv, "rb")
+   data = {"file": (csv_data, "sample_data.csv")}
+   response = client.post('/data', data=data)
+   response = client.get('/split')
+   assert response.status_code == 200   
+
+def test_input_labels_get_method(client):
+   csv = "tests/flask_app/test_data/fake_data.csv"
+   csv_data = open(csv, "rb")
+   data = {"file": (csv_data, "sample_data.csv")}
+   response = client.post('/data', data=data)
+   response = client.get('/input_labels')
+   assert response.status_code == 200 
+
+#def test_input_labels_post_method(client):
+#   csv = "tests/flask_app/test_data/fake_data.csv"
+#   csv_data = open(csv, "rb")
+#   data = {"file": (csv_data, "sample_data.csv")}
+#   response = client.post('/data', data=data)
+#   response = client.get('/input_labels')
+#   response = client.post('/input_labels', data={'drop_label':'a'})
+#   assert response.status_code == 200
+
+def test_labels_get_method(client):
+   csv = "tests/flask_app/test_data/fake_data.csv"
+   csv_data = open(csv, "rb")
+   data = {"file": (csv_data, "sample_data.csv")}
+   response = client.post('/data', data=data)
+   response = client.get('/labels')
+   assert response.status_code == 200
+
+#def test_labels_post_method(client):
+#   csv = "tests/flask_app/test_data/fake_data.csv"
+#   csv_data = open(csv, "rb")
+#   data = {"file": (csv_data, "sample_data.csv")}
+#   response = client.post('/data', data=data)
+#   response = client.get('/labels')
+#   response = client.post('/labels', data={'label':'a'})
+#   assert response.status_code == 200    
