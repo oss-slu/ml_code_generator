ac84ac535d30107de77fcd975844e72982f68c39
KD335 <k33mash@gmail/com>
2022-10-16 03:19:15 +0000

    more code cleaning

--- pandas_code/code_templates/split.py
+++ pandas_code/code_templates/split.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # split the data set into training and test data
 def get_code(args):
-   x_train, x_test, y_train, y_test = train_test_split(args[0], args[1], 
+   x_train, x_test, y_train, y_test = train_test_split(args[0], args[1],
       test_size = args[2], random_state = args[3])
    y_train = np.asarray(y_train)
    y_test = np.asarray(y_test)
