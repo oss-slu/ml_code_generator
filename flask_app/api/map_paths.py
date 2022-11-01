2e87c98445852ba2fbcc8bfd6b9a72f6781fc3dd
Tyler McMahon <tyler.mcmahon@slu.edu>
2022-11-01 02:00:31 +0000

    early addition of dictionary in flask_app.api.map_paths. Tried to implement in flask_app.api.views.py but it crashes.

--- flask_app/api/map_paths.py
+++ flask_app/api/map_paths.py
@@ -1,7 +1,4 @@
 from flask import render_template
 
-#function should check for the current_state that it is given
-#then should show the appropriate action pages
-def mapp(current_state):
-   if current_state == "upload_data":
-      return render_template('actions/actions.html', next_actions=['actions/decribe_data.html'])
+next_actions = dict()
+next_actions['upload'] = [('describe', 'actions/describe_data.html'), ('clean', 'actions/clean_data.html')]
