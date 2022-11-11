9db96b9e61b7559a0a905bf9603802f8bb125ac0
John Coffman <john.coffman@slu.edu>
2022-11-11 09:53:13 -0600

    added some functinality to google_drive with save_img and upload functions

--- flask_app/api/google_drive.py
+++ flask_app/api/google_drive.py
@@ -13,20 +13,45 @@ def build_drive_api_v3():
     credentials = build_credentials()
     return googleapiclient.discovery.build('drive', 'v3', credentials=credentials).files()
 
+def save_image(file_name, mime_type, file_data):
+    drive_api = build_drive_api_v3()
+
+    generate_ids_result = drive_api.generateIds(count=1).execute()
+    file_id = generate_ids_result['ids'][0]
+
+    body = {
+        'id': file_id,
+        'name': file_name,
+        'mimeType': mime_type,
+    }
+
+    media_body = MediaIoBaseUpload(file_data,
+                                   mimetype=mime_type,
+                                   resumable=True)
+
+    drive_api.create(body=body,
+                     media_body=media_body,
+                     fields='id,name,mimeType,createdTime,modifiedTime').execute()
+
+    return file_id
+
 def upload():
-   if flask.request.method == 'POST':
-    creds = build_credentials()
-    try: 
-        build_drive_api_v3()
-        file_metadata = {'name': 'download.jpeg'}
-        media = MediaFileUpload('download.jpeg',
-                                mimetype='image/jpeg')
-        # pylint: disable=maybe-no-member
-        file = service.files().create(body=file_metadata, media_body=media,
-                                      fields='id').execute()
-        print(F'File ID: {file.get("id")}')
-    except HTTPerror as error:
-        print(F'An error occurred: {error}')
-        file = None
-        
-   return flask.render_template('upload.html')
+    if flask.request.method == 'POST':
+        if 'file' not in flask.request.files:
+            return flask.redirect(flask.request.url)
+
+        file = flask.request.files['file']
+        if (not file):
+            return flask.redirect(flask.request.url)
+            
+        filename = secure_filename(file.filename)
+
+        fp = tempfile.TemporaryFile()
+        ch = file.read()
+        fp.write(ch)
+        fp.seek(0)
+
+        mime_type = flask.request.headers['Content-Type']
+        save_image(filename, mime_type, fp)
+
+    return flask.render_template('upload.html')
