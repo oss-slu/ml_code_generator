67c1ba62b8413090a7726998ccda3ef391c1e877
KD335 <k33mash@gmail/com>
2022-11-21 16:59:29 +0000

    fixing pylint issues

--- pandas_code/code_templates/eval_lin_reg.py
+++ pandas_code/code_templates/eval_lin_reg.py
@@ -4,3 +4,5 @@ def get_code(args):
    train_error = mean_squared_error(args[0], args[2])
    test_error = mean_squared_error(args[1], args[3])
    error_change = abs(test_error - train_error)
+   return error_change 
+   # ^ don't really need to return but pylint complained 
