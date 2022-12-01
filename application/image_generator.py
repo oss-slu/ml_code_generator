efbd11e361c6a354fb20a334c05b5ec291446c20
Haris Alam <harisalamlodhi@outlook.com>
2022-11-30 23:25:56 -0600

    Pylint issue removal

--- application/image_generator.py
+++ application/image_generator.py
@@ -5,18 +5,18 @@ class ImageGenerator(CodeParser):
    def load_images(self, images_directory):
       #Dividing the data in different batches and showing labels
       self.\
-         _parse_and_execute('load_images', [images_directory])
+         parse_and_execute('load_images', [images_directory])
 
    def validate_images(self, images_directory):
       self.\
-         _parse_and_execute('validate_images', [images_directory])
+         parse_and_execute('validate_images', [images_directory])
 
    def preprocess_images(self, images_directory): #directory after validating the images
       data = self.\
-         _parse_and_execute('preprocess_images', [images_directory])
+         parse_and_execute('preprocess_images', [images_directory])
       print("Train classes:: ", data.class_names)
       print("Number of training files:", len(data.file_paths))
-      self._save('data', data)
+      self.save('data', data)
       class_names = self.data['data'].class_names
       plt.figure(figsize=(10, 10))
       for images, labels in self.data['data'].take(random.randint(1,len(data))):
@@ -30,18 +30,18 @@ class ImageGenerator(CodeParser):
 
    def split_images(self, train_ratio, val_ratio, test_ratio):
       (train, val, test) = self.\
-         _parse_and_execute('split_images', ['data', train_ratio, val_ratio, test_ratio])
-      self._save('train', train)
-      self._save('val', val)
-      self._save('test', test)
+         parse_and_execute('split_images', ['data', train_ratio, val_ratio, test_ratio])
+      self.save('train', train)
+      self.save('val', val)
+      self.save('test', test)
       return self.data['train'], self.data['val'], self.data['test']
 
    def train_model(self, epochs):
       history = self.\
-         _parse_and_execute('images_model', ['data', 'train', 'val', epochs])
-      self._save('history', history)
+         parse_and_execute('images_model', ['data', 'train', 'val', epochs])
+      self.save('history', history)
       return self.data['history']
 
    def visualize(self, history):
       self.\
-         _parse_and_execute('visualize', ['history', history])
+         parse_and_execute('visualize', ['history', history])
