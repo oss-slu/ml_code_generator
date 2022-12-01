2c8c0eeaabc7050b096eee7e70e4a1e3d290d02c
John Coffman <john.coffman@slu.edu>
2022-12-01 09:26:03 -0600

    trying to fix pylint errors

--- flask_app/api/views.py
+++ flask_app/api/views.py
@@ -20,9 +20,9 @@ def welcome():
 def make_ipynb(code):
    template = {"cells":[{"cell_type":"code","metadata":{},"outputs":[],"source":[code]}],
    "metadata":{"language_info":{"name":"python"},"orig_nbformat":4},"nbformat":4,"nbformat_minor":2}
-   with open ("data/sample.ipynb", "w") as outfile:
+   with open ("data/sample.ipynb", mode = "w",encoding="utf-8") as outfile:
       json.dump(template,outfile)
- 
+
 def download_code():
    session['current_state'] = 'download'
    code = generator.download_code()
