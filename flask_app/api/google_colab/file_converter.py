9553a6ea057b423618621b189f768aeba1bd9ff8
John Coffman <john.coffman@slu.edu>
2022-12-05 13:32:16 -0600

    pylint errors

--- flask_app/api/google_colab/file_converter.py
+++ flask_app/api/google_colab/file_converter.py
@@ -8,7 +8,7 @@ def make_ipynb():
    file_name = "data/"+ str(date.today()) + ".ipynb"
    template = {"cells":[{"cell_type":"code","metadata":{},"outputs":[],"source":[code]}],
    "metadata":{"language_info":{"name":"python"},"orig_nbformat":4},"nbformat":4,"nbformat_minor":2}
-   if(os.path.exists(file_name)):
+   if os.path.exists(file_name):
       with open (file_name, mode = "w",encoding="utf-8") as outfile:
          json.dump(template,outfile)
    else:
