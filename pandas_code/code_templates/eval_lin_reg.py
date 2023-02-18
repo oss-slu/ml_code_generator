e000c128a5d870b032212f67fd763e0509ff7eb4
KD335 <k33mash@gmail/com>
2023-02-18 23:22:15 +0000

    Added code to print error change, test error, and train error

--- pandas_code/code_templates/eval_lin_reg.py
+++ pandas_code/code_templates/eval_lin_reg.py
@@ -4,5 +4,7 @@ def get_code(args):
    train_error = mean_squared_error(args[0], args[2])
    test_error = mean_squared_error(args[1], args[3])
    error_change = abs(test_error - train_error)
+   print("Mean Squared error on train set =", train_error)
+   print("Mean Squared error on test set =", test_error)
+   print("Difference between test error and train error =", error_change)
    return error_change
-   # ^ don't really need to return but pylint complained
