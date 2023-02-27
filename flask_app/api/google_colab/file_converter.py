4acc50a169cd1b44440faeeae07aa3bdbdd5d090
John Coffman <john.coffman@slu.edu>
2023-02-27 13:35:56 -0600

    added mounting of google drive to the generated code to later attach the dataframe from google drive to google colab

--- flask_app/api/google_colab/file_converter.py
+++ flask_app/api/google_colab/file_converter.py
@@ -6,8 +6,35 @@ from flask_app.api.generator import generator
 def make_ipynb():
    code = generator.download_code()
    file_name = "data/"+ str(date.today()) + ".ipynb"
-   template = {"cells":[{"cell_type":"code","metadata":{},"outputs":[],"source":[code]}],
-   "metadata":{"language_info":{"name":"python"},"orig_nbformat":4},"nbformat":4,"nbformat_minor":2}
+   template = {
+   "cells": [
+      {
+      "cell_type": "code",
+      "metadata": {},
+      "outputs": [],
+      "source": [
+        "from google.colab import drive\n",
+        "drive.mount('/content/drive')"
+      ],
+      },
+      {
+         "cell_type": "code",
+         "metadata": {},
+         "outputs": [],
+         "source": [
+            code
+         ]
+      }
+   ],
+   "metadata": {
+      "language_info": {
+         "name": "python"
+      },
+      "orig_nbformat": 4
+   },
+   "nbformat": 4,
+   "nbformat_minor": 2
+}
    if os.path.exists(file_name):
       with open (file_name, mode = "w",encoding="utf-8") as outfile:
          json.dump(template,outfile)
