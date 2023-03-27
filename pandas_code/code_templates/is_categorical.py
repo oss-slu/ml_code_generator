773c7eee90bc3daf15ebe84e54a53b495b85a810
KD335 <k33mash@gmail/com>
2023-03-27 01:35:27 +0000

    Fixing pylint errors part 3

--- pandas_code/code_templates/is_categorical.py
+++ pandas_code/code_templates/is_categorical.py
@@ -7,5 +7,6 @@ def is_categorical(x_data, y_data):
       if not is_numeric_dtype(x_data[col]):
          x_is_cat = True
    if not is_numeric_dtype(y_data):
-      y_is_cat = True   
+      y_is_cat = True
    return (x_is_cat, y_is_cat)
+   
